19d36de8b3351580b06b9333c17f21dac8308409
Sirajudeen <kbsiraj12@gmail.com>
2024-05-05 11:17:40 +0530

    Navbar light mode.

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import React from "react";
+import Logo from "../../public/assets/Logo.svg";
+import Enquiry from "../../public/assets/Enquiry.svg";
+import Menu from "../../public/assets/Menu.svg";
+
+const navLinks = [
+  { name: "Home" },
+  { name: "About Us" },
+  { name: "Products" },
+  { name: "Testimonials" },
+];
+
+function Navbar() {
+  return (
+    <nav className="flex w-full items-center justify-between px-[20px] py-[16px]  lg:mx-auto lg:px-10">
+      <div className="flex items-center">
+        <Image className="lg:hidden md:hidden" src={Logo} alt="Logo" />
+        <span className="hidden lg:block md:block">Grivas</span>
+      </div>
+      <div className="hidden lg:flex md:flex gap-x-[55px]">
+        {navLinks.map((item, index) => (
+          <a href="#" className="font-medium link-underline" key={index}>
+            {item.name}
+          </a>
+        ))}
+      </div>
+      <div className="flex gap-x-5">
+        <div className=" items-center flex gap-x-2">
+          <Image className=" lg:hidden md:hidden" src={Enquiry} alt="Enquiry" />
+          <button className="hidden lg:block md:block bg-orange p-2 rounded-md hover:bg-charcoal">
+            <p className=" text-white font-semibold">
+            Enquire Now
+            </p>
+          </button>
+        </div>
+        <div className=" lg:hidden md:hidden">
+          <Image src={Menu} alt="Menu" />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,19 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      orange: "#ffaa2b",
+      white: "#FFFFFF",
+      black: "#000000",
+      charcoal: "#36454F",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
